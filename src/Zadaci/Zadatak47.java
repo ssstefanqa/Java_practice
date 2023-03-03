4ee6784eb1f30d189e6301b19041723e3c593c7e
ssstefanqa <smarjanovic443@gmail.com>
2023-03-03 23:23:10 +0100

    Finished Zadatak47

--- src/Zadaci/Zadatak47.java
+++ src/Zadaci/Zadatak47.java
@@ -1,29 +1,18 @@
 package Zadaci;
 
-import java.util.ArrayList;
-import java.util.Scanner;
-
 public class Zadatak47 {
     public static void main(String[] args) {
-        // Nije uradjen!
 
         //Zadatak 47 - U main metodi pozvati metodu gde prosledjujete ime i godiste, a druga metoda stampa
         //ime korisnika i koliko ima godina
 
+        prosledjivanje("Tamara", 1999);
+        prosledjivanje("Pera Peric", 1957);
+
     }
 
-    /*---------------------------------------------------------------------------------------------------------------
-
-    public static void main(String[] args) {
-
-        //ime korisnika i koliko ima godina
-        prosledjivanje("Kristijan", 1976);
+    public static void prosledjivanje(String ime, int godiste) {
+        int starost = 2023 - godiste;
+        System.out.println("Korisnik se zove " + ime + " i ima " + starost + " godina.");
     }
-
-    public static void prosledjivanje(String a, int x) {
-        int godiste = 2022 - x;
-
-        System.out.println("Korisnik se zove " + a + " i ima " + godiste + " godina");
-    }*/
 }
-
